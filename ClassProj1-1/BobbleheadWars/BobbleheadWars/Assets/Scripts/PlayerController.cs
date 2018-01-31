5726b7fb3a50d6f12c50690800d096632acbc29f
GrahamSeanc <35589646+GrahamSeanc@users.noreply.github.com>
2018-01-30 22:35:40 -0700

    Project 1-2
    12 videos down

---
+++ ClassProj1-1/BobbleheadWars/BobbleheadWars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    public float moveSpeed = 50.0f;
+    private CharacterController characterController;
+
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+}

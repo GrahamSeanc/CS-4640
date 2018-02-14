989b45b8b173102fd91edf8f7ab05f52b8eb66b1
GrahamSeanc <35589646+GrahamSeanc@users.noreply.github.com>
2018-02-14 09:31:44 -0700

    Unfinished

---
+++ Space Shooter Unfinished/Space Shooter Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,112 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public GameObject[] spawnPoints;
+    public float upgradeMaxtimeSpawn = 7.5f;
+
+    public GUIText restartText;
+    public GUIText gameOverText;
+    public GUIText scoreText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+    private float actualUpgradeTime = 0;
+    private float currentUpgradeTime = 0;
+    private bool spawnedUpgrade = false;
+    public GameObject upgradePrefab;
+
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+        actualUpgradeTime = Random.Range(upgradeMaxtimeSpawn - 3.0f, upgradeMaxtimeSpawn);
+        actualUpgradeTime = Mathf.Abs(actualUpgradeTime);
+    }
+    void Update()
+    {
+       if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+        currentUpgradeTime += Time.deltaTime;
+
+        if (currentUpgradeTime > actualUpgradeTime)
+        {
+            if (!spawnedUpgrade)
+            {
+                int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+                GameObject spawnLocation = spawnPoints[randomNumber];
+                GameObject upgrade = Instantiate(upgradePrefab) as GameObject;
+                Upgrade upgradeScript = upgrade.GetComponent<Upgrade>();
+                upgrade.transform.position = spawnLocation.transform.position;
+                spawnedUpgrade = true;
+            }
+        }
+    }
+
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range (0,hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+
+
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+
+}
+

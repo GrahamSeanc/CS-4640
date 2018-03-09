8273189ee74a6bc53350a6d49f7e41b8abcf4fc0
GrahamSeanc <35589646+GrahamSeanc@users.noreply.github.com>
2018-03-08 21:18:24 -0700

    reduced size

---
+++ Robot Rampage/Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+
+
+    private static Game Singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+    public int enemiesLeft;
+
+	// Use this for initialization
+	void Start () {
+        Singleton = this;
+        SpawnRobots();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+}
